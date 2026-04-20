e1382cf2c8d0d1b88ea94d4e13924a5a0aab8c7f
donghyun-0503 <donghyun0503@gamil.com>
2026-04-20 09:30:07 +0900

    section1

--- section1/chapter04.js
+++ section1/chapter04.js
@@ -2,3 +2,22 @@
 let age = 17;
 
 age = 20;
+
+//2. 상수
+const birth = "2010.05.03";
+birth = "123"
+
+//3. 변수 명명 규칙(네이밍 규칙)
+//3-1. $, _외의 특수 문자는 사용할 수 없다
+let $_name;
+
+//3-2. 변수는 숫자로 시작할 수 없다
+let name1;
+let _2name;
+
+//3-3. 예약어를 사용할 수 없다
+
+//4. 변수 명명 가이드
+let salesCount = 1;
+let refundCount = 2;
+let totalSalesCount = salesCount + refundCount;
---
+++ section1/chapter05.js
@@ -0,0 +1,40 @@
+//1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+//모듈러 연산
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// console.log(1 * "hello");
+
+//2. String Type
+let myName = "이동현";
+let myLocation = "대구";
+let introduce = myName + myLocation;
+
+// console.log(introduce);
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+// console.log(introduceText)
+//템플릿 리터럴 문법
+
+//3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+//4. Null Type
+let empty = null;
+
+//5. Undefined Type
+let none;
+console.log(none);
