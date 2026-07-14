e363d05bfd9ec1ddba1ef6991f87cc1c41d62661
donghyun-0503 <donghyun100503@gmail.com>
2026-07-14 10:16:12 +0900

    리액트 기초

--- section5/src/components/Main.jsx
+++ section5/src/components/Main.jsx
@@ -1,3 +1,5 @@
+import "./Main.css";
+
 // JSX문법 주의 사항
 // 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다.(조건문, 반복문 사용 불가능)
 // 2. 숫자, 문자열, 배열값만 렌더링 된다.(true, false, undefined, null 사용 불가능)
@@ -5,17 +7,18 @@
 // 4. 최상위 태그는 반드시 하나여야만 한다.
 
 const Main = () => {
-  const number = 10;
-  const obj = { a: 1 };
-  return (
-    <main>
-      <h1>main</h1>
-      <h2>
-        {number % 2 === 0 ? "짝수" : "홀수"}
-        {obj.a}
-      </h2>
-    </main>
-  );
+  const user = {
+    name: "이동현",
+    isLogin: true,
+  };
+
+  if (user.isLogin) {
+    return <div className={"logout"}>로그아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
+
+  // return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
 };
 
 export default Main;
