18cd46748a727a50d554e1f51832e3f0a8da9441
donghyun-0503 <donghyun100503@gmail.com>
2026-07-15 19:41:14 +0900

    리액트 기초

--- section5/src/components/Button.jsx
+++ section5/src/components/Button.jsx
@@ -1,6 +1,16 @@
 const Button = ({ children, text, color = "black" }) => {
+  // 이벤트 객체
+  const onClickButton = e => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton} // 이벤트 헨들러
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
