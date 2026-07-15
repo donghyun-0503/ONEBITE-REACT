e40c2616c7b661e49053042389265f7e19d6ea05
donghyun-0503 <donghyun100503@gmail.com>
2026-07-15 19:58:42 +0900

    리액트 기초

--- section5/src/App.jsx
+++ section5/src/App.jsx
@@ -1,27 +1,33 @@
 import "./App.css";
-// import Header from "./components/Header";
-// import Main from "./components/Main";
-// import Footer from "./components/Footer.jsx";
-import Button from "./components/Button";
-import Header from "./components/Header.jsx";
-
-const buttonProps = {
-  text: "메일",
-  color: "red",
-  a: 1,
-  b: 2,
-  c: 3,
-};
+import { useState } from "react";
 
 function App() {
   //부모 컴포넌트, App 컴포넌트는 Root 컴포넌트
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
+
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header />
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </>
   );
 }
