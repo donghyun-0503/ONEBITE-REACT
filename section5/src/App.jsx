bab49a93d3eaabde39fa61fb090d8a1c84c3ab55
donghyun-0503 <donghyun100503@gmail.com>
2026-07-15 19:30:43 +0900

    리액트 기초

--- section5/src/App.jsx
+++ section5/src/App.jsx
@@ -1,15 +1,27 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer.jsx";
+// import Header from "./components/Header";
+// import Main from "./components/Main";
+// import Footer from "./components/Footer.jsx";
+import Button from "./components/Button";
+import Header from "./components/Header.jsx";
+
+const buttonProps = {
+  text: "메일",
+  color: "red",
+  a: 1,
+  b: 2,
+  c: 3,
+};
 
 function App() {
   //부모 컴포넌트, App 컴포넌트는 Root 컴포넌트
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      <Button text={"블로그"}>
+        <Header />
+      </Button>
     </>
   );
 }
