fcda0cd897015e7625b1bc07a378fba5b7ee5b4c
donghyun-0503 <donghyun100503@gmail.com>
2026-07-20 13:54:59 +0900

    리액트 기초

--- section5/src/components/Register.jsx
+++ section5/src/components/Register.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 // 간단한 회원가입 폼
 // 1. 이름
 // 2. 생년월일
 // 3. 국적
 // 4. 자기소개
+
 const Register = () => {
   const [input, setInput] = useState({
     name: "",
@@ -11,19 +12,35 @@ const Register = () => {
     country: "",
     bio: "",
   });
+  const countRef = useRef(0);
+  const inputRef = useRef();
 
   const onChange = e => {
-    console.log(e.target.name, e.target.value);
+    countRef.current++;
+    console.log(countRef.current);
     setInput({
       ...input,
-      name: e.target.value,
+      [e.target.name]: e.target.value,
     });
+  };
+
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 DOM 요소에 포커스
+      inputRef.current.focus();
+    }
   };
 
   return (
     <div>
       <div>
-        <input name="name" value={input.name} onChange={onChange} placeholder={"이름"} />
+        <input
+          ref={inputRef}
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
       </div>
 
       <div>
@@ -42,6 +59,8 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
+
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
