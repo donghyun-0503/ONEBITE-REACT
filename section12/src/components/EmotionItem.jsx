7ce79c8fb3db5f117f9a7d85083f9be723084ac8
donghyun-0503 <donghyun100503@gmail.com>
2026-07-31 15:25:55 +0900

    리액트 기초

--- section12/src/components/EmotionItem.jsx
+++ section12/src/components/EmotionItem.jsx
@@ -1,9 +1,12 @@
 import "./EmotionItem.css";
 import { getEmotionImage } from "../util/get-emotion-images.js";
 
-const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+const EmotionItem = ({ emotionId, emotionName, isSelected, onClick }) => {
   return (
-    <div className={`EmotionItem EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}>
+    <div
+      onClick={onClick}
+      className={`EmotionItem EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}
+    >
       <img className={"emotion_img"} src={getEmotionImage(emotionId)} alt={emotionName} />
       <div className={"emotion_name"}>{emotionName}</div>
     </div>
