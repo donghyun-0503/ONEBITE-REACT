e8930b10f7f5fd0691baefe459e43ffb3cdb351d
donghyun-0503 <donghyun100503@gmail.com>
2026-07-31 10:21:39 +0900

    컴포넌트

---
+++ section12/src/components/EmotionItem.jsx
@@ -0,0 +1,13 @@
+import "./EmotionItem.css";
+import { getEmotionImage } from "../util/get-emotion-images.js";
+
+const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+  return (
+    <div className={`EmotionItem EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}>
+      <img className={"emotion_img"} src={getEmotionImage(emotionId)} alt={emotionName} />
+      <div className={"emotion_name"}>{emotionName}</div>
+    </div>
+  );
+};
+
+export default EmotionItem;
